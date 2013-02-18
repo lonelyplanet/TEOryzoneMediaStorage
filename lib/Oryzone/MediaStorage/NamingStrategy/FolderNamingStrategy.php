5d8e5f31341ee3b4256c8c3b92b1daaa7049102d
Javier F. Escribano <javier@touristeye.com>
2013-02-18 11:58:17 -0500

    Add folder naming strategy
    The photos will be saved in folders like  "context / variant / filename"

---
+++ lib/Oryzone/MediaStorage/NamingStrategy/FolderNamingStrategy.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Oryzone/MediaStorage package.
+ *
+ * (c) Javier F. Escribano <javier@touristeye.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Oryzone\MediaStorage\NamingStrategy;
+
+use Gaufrette\Filesystem;
+
+use Oryzone\MediaStorage\Model\MediaInterface,
+    Oryzone\MediaStorage\Variant\VariantInterface,
+    Oryzone\MediaStorage\Exception\InvalidArgumentException;
+
+class FolderNamingStrategy extends NamingStrategy
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function generateName(MediaInterface $media, VariantInterface $variant, Filesystem $filesystem)
+    {
+        if ( !$media->getFilename() ) $media->setFilename(uniqid());
+
+        return $media->getContext().'/'.$variant->getName().'/' . $media->getFilename();
+    }
+}
